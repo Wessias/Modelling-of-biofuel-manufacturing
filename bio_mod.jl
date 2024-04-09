f0ebf3040c100917fa9d97cfb9c588f6f52dc70b
Wessias <viggowessner@gmail.com>
2024-04-09 18:09:46 +0200

    fina namn

--- bio_mod.jl
+++ bio_mod.jl
@@ -27,16 +27,31 @@ function build_bio_model(data_file::String)
   @variable(model, V[j] >= 0) # Volume (liters) of j produced [blend]
 
   #Maximize profit <=> maximize "Earnt - cost of petrol and methanol"
-  @objective(model, Max, sum((1-T)[j]*P[j]*V[j] - P_p*(1-B[j])V[j] for j in J) - sum(P_m*0.2*O[i]*Y[i]*A[i] for i in I))
+  @objective(model, Max, sum((1-T)[j] * P[j] * V[j] - P_p * (1 - B[j])V[j] for j in J) - sum(P_m * 0.2 * O[i] * Y[i] * A[i] for i in I))
 
 
   #Constraints
-  @constraint(model, 0.9*sum(B[j]*V[j] for j in J) == sum(O[i]*Y[i]*A[i] for i in I)) #Proudction of biodisel and vegetable oil should match
-  @constraint(model, sum(V[j] j in J) >= Fuel_demand) #Need to meet oil demand of 280 000 liters
-  @constraint(model, sum(A[i] for i in I) <= Area_max) #Limited area to farm
-  @constraint(model, sum(A[i]*W[i] for i in I) <= Water_max) #Limited amount of water we can use
-  @constraint(model, sum((1-B[j])V_j for j in J) <= Petrol_max) #Limited amount of petrol diesel available
-  
+  # Constraints
+  @constraint(model, biodiesel_production_constraint,
+    0.9 * sum(B[j] * V[j] for j in J) == sum(O[i] * Y[i] * A[i] for i in I)
+  )  # Production of biodiesel and vegetable oil should match
+
+  @constraint(model, oil_demand_constraint,
+    sum(V[j] for j in J) >= Fuel_demand
+  )  # Need to meet oil demand of Fuel_demand liters
+
+  @constraint(model, area_limit_constraint,
+    sum(A[i] for i in I) <= Area_max
+  )  # Limited area available for farming
+
+  @constraint(model, water_limit_constraint,
+    sum(A[i] * W[i] for i in I) <= Water_max
+  )  # Limited amount of water that can be used
+
+  @constraint(model, petrol_limit_constraint,
+    sum((1 - B[j]) * V[j] for j in J) <= Petrol_max
+  )  # Limited amount of petrol diesel available
+
 
   return model, A, V
 end
